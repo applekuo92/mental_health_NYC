29ccef207314bf736d15d810fb079bacac028c2e
maurafitzgerald2 <maurafitzgerald2@gmail.com>
2016-12-12 15:22:19 -0500

    fix again

--- MakeBoW.R
+++ MakeBoW.R
@@ -29,6 +29,7 @@
 # corpus <- rbind(corp1, corp2, corp3)
 
 
+setwd("~/mental_health_NYC")
 
 library(data.table)
 library(tm)
@@ -39,20 +40,21 @@ depressed <- Corpus(DirSource("depress_corpus/"))
 names(depressed) <- paste0("dep_",names(depressed))
 motivate <- Corpus(DirSource("good/"))
 names(motivate) <- paste0("good_",names(motivate))
+newyork <- Corpus(DirSource("NewYork/"))
+names(newyork) <- paste0("NewYork_",names(newyork))
 
-p_corp <- c(control, depressed, motivate)
 
-tdm <- TermDocumentMatrix(p_corp, control=list(removePunctuation = TRUE, removeNumbers=TRUE, tolower=FALSE, stopwords = TRUE))
-tdm$dimnames$Terms
-inspect(tdm)
+p_corp <- c(control, depressed, motivate, newyork)
 
-dtm <- DocumentTermMatrix(p_corp, control=list(removePunctuation = TRUE, removeNumbers=TRUE, tolower=TRUE, stopwords = TRUE))
+
+dtm <- DocumentTermMatrix(p_corp, control=list(removePunctuation = TRUE, removeNumbers=TRUE, tolower=TRUE, stopwords = TRUE, stemDocument = TRUE))
 dtm$dimnames$Terms
 
-dtms <- removeSparseTerms(dtm, 0.99)
-dtms$dimnames$Terms
+#dtms <- removeSparseTerms(dtm, 0.997)
+dtms <- dtm
 
 bow <- (as.matrix(dtms)) 
+dim(bow)
 
 #Classes
 bow_metadata <- as.data.frame(row.names(bow))
@@ -64,4 +66,22 @@ bow_metadata[which(bow_metadata$corpus %like% "dep"),]$subreddit_class <- "Depre
 ages <- read.csv("Age_tagged_corpora.csv")
 bow_metadata<- as.data.table(merge(bow_metadata, ages, by="corpus", all.x=TRUE))
 
-saveRDS( list(dictionary= dtms$dimnames$Terms, removePunctuation = TRUE, removeNumbers=TRUE, tolower=FALSE, stopwords = TRUE), "dictionary.RDS")
+#Save Dictionary
+saveRDS( list(dictionary= dtms$dimnames$Terms, removePunctuation = TRUE, removeNumbers=TRUE, tolower=FALSE, stopwords = TRUE, stemDocument = TRUE), "dictionary.RDS")
+
+#Build Models
+
+#Principal Components
+components<-prcomp(bow)
+plot(components)
+summary(components)
+head(components$x[,1:500])
+
+saveRDS(components,"PCA.RDS")
+
+comp <- predict(components, bow)[,1:1000]
+comp$isdep <- factor(bow_metadata$subreddit_class)
+
+mylogit <- glm(isdep ~ ., data = comp, family = "binomial")
+bow2<-as.data.frame(bow)
+summary(bow)
